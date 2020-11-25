7ec47affd00dbce377e157dda95b12068d237963
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-11-25 23:15:25 +0800

    try to fix crontab again

--- yycode/cron.py
+++ yycode/cron.py
@@ -18,26 +18,21 @@ def update_vistor():
     for i in vistor_list:
         print(f'ip为 : {i.ip}')
         url = f"http://www.ip-api.com/json/{i.ip}?lang=zh-CN"
-        res = requests.get(url)
-        ip_message = res.json()
-        print(ip_message)
         try:
-            # now = time.strftime()
+            res = requests.get(url)
+            ip_message = res.json()
+            print(ip_message)
+            # now = time.strftime()  # todo 加了时间后会报错
             if ip_message.get('status') == 'success':
                 i.country = ip_message.get('country')
-                print(i.country)
                 i.city = ip_message.get('city')
-                print(i.city)
                 i.ip_as = ip_message.get('as')
-                print(i.ip_as)
                 i.isp = ip_message.get('isp')
-                print(i.isp)
-                print("获取访问者信息成功")
                 i.save()
-                # print(f"保存耗时{time.strftime() - now}")
+                print("*******************保存成功******************")
             elif ip_message.get('status') == 'fail':
                 print("请求失败")
             else:
-                print("更新失败")
+                pass
         except:
             print("未知异常")
