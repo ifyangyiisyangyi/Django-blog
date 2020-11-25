80ecbef7503777cb913805d8c20bbe6e1f0c0bd1
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-11-25 22:11:32 +0800

    fix crontab

--- yycode/cron.py
+++ yycode/cron.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from django.core.mail import send_mail
 
@@ -13,22 +15,19 @@ def send_email():
 # 更新访问者信息
 def update_vistor():
     vistor_list = Vistor.objects.filter(count=1, country='')
-    print(vistor_list[0])
     for i in vistor_list:
         print(f'ip为 : {i.ip}')
         url = f"http://www.ip-api.com/json/{i.ip}?lang=zh-CN"
-        try:
-            res = requests.get(url)
-            ip_message = res.json()
-            if ip_message.get('status') == 'success':
-                i.country = ip_message.get('country')
-                i.city = ip_message.get('city')
-                i.ip_as = ip_message.get('as')
-                i.isp = ip_message.get('isp')
-                i.save()
-            elif ip_message.get('status') == 'fail':
-                print("请求失败")
-            else:
-                pass
-        except:
+        res = requests.get(url)
+        ip_message = res.json()
+        if ip_message.get('status') == 'success':
+            i.country = ip_message.get('country')
+            i.city = ip_message.get('city')
+            i.ip_as = ip_message.get('as')
+            i.isp = ip_message.get('isp')
+            time.sleep(1)
+            i.save()
+        elif ip_message.get('status') == 'fail':
+            print("请求失败")
+        else:
             print("更新失败")
