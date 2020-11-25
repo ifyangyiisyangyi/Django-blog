fd487316edd84f50944ac6a7991c551df67b4b2b
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-11-25 17:55:31 +0800

    add update vistor message crontab

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
 
 # Create your tests here.
+from TestModel.models import Vistor
+
+
+class VistorTestCase(TestCase):
+    """
+    def setUp(self)：用来初始化环境，包括创建初始数据，或做一些其他准备工作
+    def test_xxx(self)：xxx可以是任何东西，以test_开头的方法，都会被django认为是需要测试的方法，跑测试时会被执行。
+        注：每个需要被测试的方法都是相互独立的
+    def tearDown(self)：跟setUp相对，用来清理测试环境和测试数据（在django中可以不关心这个）
+    """
+
+    def setUp(self):
+        print("setUp")
+        Vistor.objects.create(
+            ip='223.70.230.166',
+            user_agent='',
+            count='1',
+            city='',
+            country='',
+            ip_as='',
+            isp=''
+        )
+        Vistor.objects.create(
+            ip='223.70.230.111',
+            user_agent='',
+            count='1',
+            city='',
+            country='',
+            ip_as='',
+            isp=''
+        )
+        Vistor.objects.create(
+            ip='223.70.230.111',
+            user_agent='',
+            count='0',
+            city='',
+            country='',
+            ip_as='',
+            isp=''
+        )
+
+    def test_filter(self):
+        print('test_filter')
+        vistor = Vistor.objects.filter(count=1, user_agent = '')
+        print(f':{vistor}')
+        for i in vistor:
+            print(i)
+            print(i.ip)
+            i.country = 'cn'
+            i.city = 'bj'
+            i.save()
+            print(i.city)
+        print(Vistor.objects.filter(city='bj'))
+        self.assertEqual(vistor.count(), 2, "不相等")
--- yycode/cron.py
+++ yycode/cron.py
@@ -1,4 +1,7 @@
+import requests
 from django.core.mail import send_mail
+
+from TestModel.models import Vistor
 
 
 def send_email():
@@ -7,5 +10,21 @@ def send_email():
     print(res)
 
 
+# 更新访问者信息
 def update_vistor():
-    pass
+    vistor_list = Vistor.objects.filter(count=1, country='')
+    for i in vistor_list:
+        url = f"http://www.ip-api.com/json/{i.ip}?lang=zh-CN"
+        try:
+            res = requests.get(url)
+            ip_message = res.json()
+            if ip_message.get('status') == 'success':
+                i.country = ip_message.get('country')
+                i.city = ip_message.get('city')
+                i.ip_as = ip_message.get('as')
+                i.isp = ip_message.get('isp')
+            elif ip_message.get('status') == 'fail':
+                print("请求失败")
+            i.save()
+        except:
+            print("更新失败")
