7d47ac1f52ae835cd19a540680f79e4b795ef40f
ifyangyiisyangyi <yangyi7351598@gmail.com>
2020-12-06 22:44:57 +0800

    add tool

--- tool/urls.py
+++ tool/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Cbvdemo, Toolview
+from .views import Cbvdemo, Toolview, commdty_code_make_up
 
+app_name = 'tool'
 urlpatterns = [
-    path('Cbvdemo', Cbvdemo.as_view()),
     path('', Toolview, name='total'),
+    path('Cbvdemo', Cbvdemo.as_view()),
+    path('commdty_code_make_up/', commdty_code_make_up, name='commdty_code_make_up'),  # 商品编码自动补齐
 
 ]
--- tool/views.py
+++ tool/views.py
@@ -3,14 +3,17 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
+
 def Toolview(request):
     return render(request, 'tool/tool.html')
+
 
 class Cbvdemo(View):
     context = {
         'code': 0,
         'msg': ''
     }
+
     def get(self, request):
         self.context['msg'] = '这是Cbvdemo get请求'
         return JsonResponse(self.context)
@@ -19,4 +22,7 @@ class Cbvdemo(View):
         self.context['msg'] = '这是Cbvdemo post请求'
         return JsonResponse(self.context)
 
+
+def commdty_code_make_up(request):
+    return render(request, 'tool/commdty_code_make_up.html')
 
